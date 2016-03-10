790f7835e10026cd2785218495b6f7c6d916496a
Arthur Maltson <arthur@maltson.com>
2016-03-09 22:01:56 -0500

    Fix back button by using replaceWith
    - also call super as per best practice.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel() {
-    this.transitionTo('orgs');
+    this._super(...arguments);
+    this.replaceWith('orgs');
   }
 });
